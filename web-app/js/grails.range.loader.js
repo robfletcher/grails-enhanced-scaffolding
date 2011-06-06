e56d12ff7268b2d10d0f0343403f6ce367c4b0cd
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-06-06 08:37:46 +0100

    Revert "initialise jquery slider if browser does not support input[type=range]"
    This reverts commit 69cc4af9bf45fc63d1ace6e76f500163d315c088.

--- web-app/js/grails.range.loader.js
+++ web-app/js/grails.range.loader.js
@@ -2,19 +2,5 @@
 	// if the browser supports range inputs use them instead of selects for range properties
 	if (Modernizr.inputtypes.range) {
 		$('select.range').grailsRange();
-	} else if (jQuery.ui.slider != undefined) {
-		$('select.range').each(function() {
-			var select = $(this);
-			var sliderDiv = $('<div/>').insertAfter(select);
-			sliderDiv.slider({
-				range: 'min',
-				min: parseInt(select.attr('min')),
-				max: parseInt(select.attr('max')),
-				value: parseInt(select.val()),
-				change: function(event, ui) {
-					select.val('' + ui.value);
-				}
-			});
-		});
 	}
 })(jQuery);
