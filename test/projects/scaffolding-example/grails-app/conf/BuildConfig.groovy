29509d400154d113af4d47631adeeb921890920e
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-11-17 05:00:01 +0000

    updated cf stuff

--- test/projects/scaffolding-example/grails-app/conf/BuildConfig.groovy
+++ test/projects/scaffolding-example/grails-app/conf/BuildConfig.groovy
@@ -5,10 +5,10 @@ grails.project.dependency.resolution = {
     inherits "global"
     log "warn"
     repositories {
+        mavenLocal()
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        mavenLocal()
         mavenCentral()
     }
     dependencies {
@@ -20,14 +20,15 @@ grails.project.dependency.resolution = {
 	plugins {
 		compile ":cache-headers:1.1.5"
 		compile ":cached-resources:1.0"
-//		compile ":enhanced-scaffolding:1.0.BUILD-SNAPSHOT"
+//		compile ":enhanced-scaffolding:1.0.RC2"
 		compile ":fixtures:1.0.7"
 		compile ":hibernate:$grailsVersion"
 		compile ":jquery:1.6.1.1"
 		compile ":jquery-ui:1.8.10.1"
 		compile ":modernizr:1.7.2"
 		compile ":resources:1.0.2"
-		build ":cloud-foundry:1.0.0.M3"
+		build ":cloud-support:latest.release"
+		build ":cloud-foundry:latest.release"
 		build ":tomcat:$grailsVersion"
 		test ":spock:0.6-SNAPSHOT"
 		test ":geb:0.6.1"
